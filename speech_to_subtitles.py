9056cc622ebe070ae3ef02fa290689a51da8176a
SeanCarlson96 <carlsonseanr@gmail.com>
2023-03-14 18:23:40 -0500

    git lfs initd

--- speech_to_subtitles.py
+++ speech_to_subtitles.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0f77f094b41adf0df42ad6e2fbbd63a9aaa662ba78f31bbc958ab688349c507
-size 1865
+oid sha256:9832c7063908290cb73849b1f4c36d9e7ba18bbc306d80af8394cba5c8feda6c
+size 1764

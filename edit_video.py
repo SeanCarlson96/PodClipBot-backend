1b68ee702781038e7b573f6e6a8ae26fe278a563
SeanCarlson96 <carlsonseanr@gmail.com>
2023-03-15 20:07:44 -0500

    whisperx working

--- edit_video.py
+++ edit_video.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b52259d73b727a58f8652b63f935cedc67620311bb1ebd98f1c96fdbcfb0819
-size 936
+oid sha256:be666b1ce8a247acadbdde85962ea3e15bb850ab18b12488139a609d121cdd59
+size 939
---
+++ whisperx_to_subtitles.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd7b0c1609b0c0ef8d368dbaf3b7fc31612e337ed85fe9fc54d4f764ff175f23
+size 2270

4e1e45db41f921d221b42cf862e02fc240f9fbfc
SeanCarlson96 <carlsonseanr@gmail.com>
2023-03-21 17:47:37 -0500

    segment split if time between words too much

--- whisperx_to_subtitles.py
+++ whisperx_to_subtitles.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cd39586c7dadb4e3a72819603e5fb3f08925dfc6d1cee64ba39c7287de4725e
-size 3027
+oid sha256:d9711288d4e3a61a55b8282e1f4989a8788dbd900d287bb3edc6adff020abbb5
+size 3751

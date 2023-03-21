90f1ca51be277c0e87dd822fc848b1687813e6ee
SeanCarlson96 <carlsonseanr@gmail.com>
2023-03-21 17:29:12 -0500

    update comment

--- whisperx_to_subtitles.py
+++ whisperx_to_subtitles.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e1e1b86e9aed743753529d488d3a3c990a79e258fb6bdd8bab5a91dc345d0be
-size 2900
+oid sha256:2cd39586c7dadb4e3a72819603e5fb3f08925dfc6d1cee64ba39c7287de4725e
+size 3027

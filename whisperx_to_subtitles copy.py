620fda44d9d2cc448019e5d0fea0da3a79094b15
SeanCarlson96 <carlsonseanr@gmail.com>
2023-03-21 17:19:14 -0500

    segments from char count

---
+++ whisperx_to_subtitles copy.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:972bb5f989952855e58ea8bb061acaaa61c67a1b04b85581498f6dab64ccec6c
+size 2374
--- whisperx_to_subtitles.py
+++ whisperx_to_subtitles.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd7b0c1609b0c0ef8d368dbaf3b7fc31612e337ed85fe9fc54d4f764ff175f23
-size 2270
+oid sha256:2e1e1b86e9aed743753529d488d3a3c990a79e258fb6bdd8bab5a91dc345d0be
+size 2900

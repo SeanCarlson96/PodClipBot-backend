c52670793159f3eb78b02ee4552427bf9ae3bc14
SeanCarlson96 <carlsonseanr@gmail.com>
2023-03-24 17:15:48 -0500

    background music function working

--- edit_video.py
+++ edit_video.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1f06c71a3263e5c9d89f2a3f071a7c693e73a54fe6a2640d45c466601bf8cf5
-size 1150
+oid sha256:89be85aa752cd1863745114a8f137a7e7b3cba76c9b291548de05a5b93fee059
+size 1162
---
+++ test_music.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:deaaffa884124494e385e2e6470646f2d1b3b101991ed61d7f2abdf35c9c0e7a
+size 369

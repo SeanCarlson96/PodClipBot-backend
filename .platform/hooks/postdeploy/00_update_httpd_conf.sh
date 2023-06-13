44862e72e3559086c8edcde03ca05d37764fefe1
SeanCarlson96 <carlsonseanr@gmail.com>
2023-06-13 17:50:23 -0500

    try it

--- .platform/hooks/postdeploy/00_update_httpd_conf.sh
+++ .platform/hooks/postdeploy/00_update_httpd_conf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo '' >> /etc/httpd/conf/httpd.conf
-echo 'IncludeOptional /opt/elasticbeanstalk/support/conf.d/*.conf' >> /etc/httpd/conf/httpd.conf
+echo -e '\nIncludeOptional /opt/elasticbeanstalk/support/conf.d/*.conf' >> /etc/httpd/conf/httpd.conf
 systemctl restart httpd

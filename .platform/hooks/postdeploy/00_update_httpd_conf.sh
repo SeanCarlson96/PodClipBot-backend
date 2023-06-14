fbe21b6958bf7804390f6acc3d8c274d4543e637
SeanCarlson96 <carlsonseanr@gmail.com>
2023-06-13 19:40:47 -0500

    test

--- .platform/hooks/postdeploy/00_update_httpd_conf.sh
+++ .platform/hooks/postdeploy/00_update_httpd_conf.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-{
-echo "Adding new line and IncludeOptional to httpd.conf"
-echo '#----------' >> /etc/httpd/conf/httpd.conf
+echo '' >> /etc/httpd/conf/httpd.conf
 echo 'IncludeOptional /opt/elasticbeanstalk/support/conf.d/*.conf' >> /etc/httpd/conf/httpd.conf
-echo "Finished adding lines. Here's the end of the httpd.conf file:"
-tail -n 5 /etc/httpd/conf/httpd.conf
-
-echo "Restarting httpd"
 systemctl restart httpd
-echo "Finished restarting httpd"
-} >> /tmp/debug.log 2>&1

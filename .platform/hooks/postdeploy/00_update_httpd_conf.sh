0447107fe056a95bc3ce62406ecaf901babdca4d
SeanCarlson96 <carlsonseanr@gmail.com>
2023-06-13 20:20:03 -0500

    should be proper format

--- .platform/hooks/postdeploy/00_update_httpd_conf.sh
+++ .platform/hooks/postdeploy/00_update_httpd_conf.sh
@@ -2,5 +2,5 @@
 
 # Create and write to websocket_proxy.conf
 echo "LoadModule proxy_wstunnel_module modules/mod_proxy_wstunnel.so
-      ProxyPass /socket.io/ ws://127.0.0.1:8000/socket.io/
-      ProxyPassReverse /socket.io/ ws://127.0.0.1:8000/socket.io/" >> /etc/httpd/conf.d/websocket_proxy.conf
+ProxyPass /socket.io/ ws://127.0.0.1:8000/socket.io/
+ProxyPassReverse /socket.io/ ws://127.0.0.1:8000/socket.io/" >> /etc/httpd/conf.d/websocket_proxy.conf

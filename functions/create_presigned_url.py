e0f3fd08d18485f59534324efbeb895067b0155f
SeanCarlson96 <carlsonseanr@gmail.com>
2023-06-20 12:19:20 -0500

    updated signature version

--- functions/create_presigned_url.py
+++ functions/create_presigned_url.py
@@ -2,6 +2,7 @@ import os
 import boto3
 from botocore.exceptions import NoCredentialsError
 from boto3.s3.transfer import TransferConfig, S3Transfer
+from botocore.client import Config
 
 def progress_callback(bytes_transferred):
     # This function will be called every time progress is made.
@@ -25,7 +26,7 @@ def retreive_video_file(video_name, destination_dir):
 
 def create_presigned_url(bucket_name, object_name, expiration=10000):
     """Generate a presigned URL S3 PUT request"""
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3', config=Config(signature_version='s3v4'))
     try:
         response = s3_client.generate_presigned_url('put_object',
                                                     Params={'Bucket': bucket_name,

0cced4575b4e65f92aac204b0efa8c551c2077d8
SeanCarlson96 <carlsonseanr@gmail.com>
2023-06-20 12:32:18 -0500

    corrected region

--- functions/create_presigned_url.py
+++ functions/create_presigned_url.py
@@ -26,7 +26,7 @@ def retreive_video_file(video_name, destination_dir):
 
 def create_presigned_url(bucket_name, object_name, expiration=10000):
     """Generate a presigned URL S3 PUT request"""
-    s3_client = boto3.client('s3', config=Config(signature_version='s3v4'))
+    s3_client = boto3.client('s3', region_name='us-east-2', config=Config(signature_version='s3v4'))
     try:
         response = s3_client.generate_presigned_url('put_object',
                                                     Params={'Bucket': bucket_name,
